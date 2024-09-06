98109431ce8b0f648e78b9b2dc1c96bb0b999838
andre <andremorais0000@gmail.com>
2024-09-05 23:31:19 -0300

    created the map functionality for the project

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const list = document.querySelector("ul")
-let myLi = ``
 
-    function showAll() {
-        menuOptions.forEach( products => {
-            myLi += `
+function showAll(productsarray) {
+    let myLi = ``
+
+    productsarray.forEach(products => {
+        myLi += `
                     <li class="products">
                         <img src=./${products.src}>
                         <p>${products.name}</p>
                         <p class="price">R$ ${products.price}</p>
                     </li> 
             `
-            list.innerHTML = myLi
-        });
-    }
+        list.innerHTML = myLi
+    });
+}
+
+function mapAll() {
+    const newPrices = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9
+    }))
+    showAll(newPrices)
+}
