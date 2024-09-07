25282178e84f3a830b3e105cc774ffafed6ff52d
andre <andremorais0000@gmail.com>
2024-09-06 21:31:16 -0300

    upgraded the project

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const list = document.querySelector("ul")
 
+function formactValue(value) {
+    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+}
+
 function showAll(productsarray) {
     let myLi = ``
 
@@ -8,7 +12,7 @@ function showAll(productsarray) {
                     <li class="products">
                         <img src=./${products.src}>
                         <p>${products.name}</p>
-                        <p class="price">R$ ${products.price}</p>
+                        <p class="price">R$ ${formactValue(products.price)}</p>
                     </li> 
             `
         list.innerHTML = myLi
@@ -23,15 +27,15 @@ function mapAll() {
     showAll(newPrices)
 }
 
-function sumAll(){
+function sumAll() {
     const sumAllItems = menuOptions.reduce((acc, current) => acc + current.price, 0)
 
-    list.innerHTML = `<li><p>A soma total dos itens é: <br> R$ ${sumAllItems}</p></li>`
+    list.innerHTML = `<li><p>A soma total dos itens é: <br> R$ ${formactValue(sumAllItems)}</p></li>`
 }
 
 function filterAll() {
     const filterVeganItems = menuOptions.filter((products) => products.vegan)
 
     showAll(filterVeganItems)
-    
+
 }
